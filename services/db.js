4f4981f101eb26bf64435e20992970170500a8c6
BjarniGod <bgjonsson@mail.fhsu.edu>
2023-10-21 08:47:56 -0500

    Adding a connection pool to database

--- services/db.js
+++ services/db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2/promise');
 const config = require('../config');
 
-async function query(sql, params) {
-  const connection = await mysql.createConnection(config.db);
-  const [results, ] = await connection.execute(sql, params);
+const pool = mysql.createPool(config.db);
 
-  return results;
+async function query(sql, params) {
+  const connection = await pool.getConnection();
+  try {
+    const [results, ] = await connection.execute(sql, params);
+    return results;
+  } finally {
+    connection.release(); // Release the connection back to the pool
+  }
 }
 
 module.exports = {
